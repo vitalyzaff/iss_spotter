83b5ba82cd41e5287aa7520e328f4da93352a769
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-06 20:41:36 +0000

    coordinates function

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const  {fetchMyIP } = require('./iss');
+const  {fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP('173.246.139.190', (error, geo) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , geo);
 });
--- iss.js
+++ iss.js
@@ -15,5 +15,21 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    const { latitude, longitude } = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    callback(null, { latitude, longitude });
+  });
+};
 
-module.exports = { fetchMyIP };
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
